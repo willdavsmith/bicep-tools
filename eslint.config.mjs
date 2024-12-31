e56a5affd1d36b0f2c5a2b26c5be24a9f10138ae
Ryan Nowak <nowakra@gmail.com>
2024-12-31 15:45:18 -0800

    Lets use webpack (#10)
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ export default tseslint.config(
     ignores: [
       '**/jest.config.js',
       '**/eslint.config.mjs',
+      '**/webpack.config.js',
       '**/dist/**',
       '**/bicep-types/**',
     ],
